e31f076cee779f1777c40afd84bd4eeb7842d648
shockham <samuel.hockham@gmail.com>
2020-01-31 09:27:48 +0000

    Add init vis and audio context

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,52 @@
-use infuse::{RenderItem, Renderer};
+use std::collections::HashMap;
+
+use infuse::{request_animation_frame, RenderItem, Renderer, Uniform};
+use instant;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+use web_sys::{AudioContext};
+
+const VERT: &str = include_str!("./shaders/vert.glsl");
+const FRAG: &str = include_str!("./shaders/frag.glsl");
 
 #[wasm_bindgen(start)]
 pub fn start() -> Result<(), JsValue> {
-    let renderer = Renderer::new()?;
+    let mut renderer = Renderer::new()?;
+
+    let start_time = instant::now();
+
+    // add a shader that will use the uniform
+    renderer.add_shader("colour".into(), VERT.into(), FRAG.into())?;
+
+    // create the uniforms for the render item
+    let mut uniforms = HashMap::new();
+    uniforms.insert("time".to_string(), Uniform::Float(start_time as f32));
+
+
+    // web audio init
+    let ctx = web_sys::AudioContext::new()?;
+
 
     let render_item = RenderItem::new(
         vec![
             -1.0, -1.0, 0.0, 1.0, -1.0, 0.0, 1.0, 1.0, 0.0, -1.0, 1.0, 0.0, -1.0, -1.0, 0.0, 1.0,
             1.0, 0.0,
         ],
-        "default".into(),
-        None,
+        "colour".into(),
+        Some(uniforms),
     );
 
-    let render_items = vec![render_item];
+    let mut render_items = vec![render_item];
 
-    renderer.draw(&render_items)?;
+    request_animation_frame!({
+        let tick_time = instant::now();
+        render_items[0].set_uniform(
+            "time".to_string(),
+            Uniform::Float((tick_time / 500f64) as f32),
+        );
+        renderer.draw(&render_items).unwrap();
+    });
 
     Ok(())
 }
